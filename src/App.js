881bbcd50c6c3dce2797fb6432adad36c5eddca9
mike wakerly <opensource@hoho.com>
2020-12-26 15:14:39 -0500

    styles: add bulma

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import MapView from './MapView';
+import "./App.scss";
+import MapView from "./MapView";
 
 function App() {
   return (
     <div className="App">
-      <MapView />
+      <div className="columns is-gapless">
+        <div className="column is-four-fifths">
+          <MapView />
+        </div>
+        <div className="column">
+          <h3>AirDash!</h3>
+        </div>
+      </div>
     </div>
   );
 }
