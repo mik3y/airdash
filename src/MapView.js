ea4c3ae8590a476b9d078e51f947c161ab4d2b3d
mike wakerly <opensource@hoho.com>
2020-12-31 17:41:34 -0500

    MapView: add layer selector
    Closes #6

--- src/MapView.js
+++ src/MapView.js
@@ -1,5 +1,11 @@
 import React, { useContext } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  LayersControl,
+} from "react-leaflet";
 import "./MapView.scss";
 
 import DataHubContext from "./DataHubContext";
@@ -18,43 +24,45 @@ L.Icon.Default.mergeOptions({
 const MapView = (props) => {
   const { entities } = useContext(DataHubContext);
 
-  const markers = Object.values(entities).map((entity) => {
-    const { type, id, lat, lon } = entity;
-    if (!lat || !lon) {
+  const markers = Object.values(entities)
+    .map((entity) => {
+      const { type, id, lat, lon } = entity;
+      if (!lat || !lon) {
+        return null;
+      }
+      if (type === "ADSB") {
+        return (
+          <Marker
+            key={id}
+            position={[lat, lon]}
+            icon={PlaneIcon(entity.adsbData)}
+          >
+            <Popup>
+              <div>
+                <h2>{entity.adsbData.flight}</h2>
+              </div>
+            </Popup>
+          </Marker>
+        );
+      } else if (type === "AIS") {
+        return (
+          <Marker
+            key={id}
+            position={[lat, lon]}
+            icon={BoatIcon(entity.aisData)}
+          >
+            <Popup>
+              <div>
+                {entity.aisData.name && <h2>{entity.aisData.name}</h2>}
+                <h3>{entity.aisData.mmsi}</h3>
+              </div>
+            </Popup>
+          </Marker>
+        );
+      }
       return null;
-    }
-    if (type === 'ADSB') {
-      return (
-        <Marker
-          key={id}
-          position={[lat, lon]}
-          icon={PlaneIcon(entity.adsbData)}
-        >
-          <Popup>
-            <div>
-              <h2>{entity.adsbData.flight}</h2>
-            </div>
-          </Popup>
-        </Marker>
-      );
-    } else if (type === 'AIS') {
-      return (
-        <Marker
-          key={id}
-          position={[lat, lon]}
-          icon={BoatIcon(entity.aisData)}
-        >
-          <Popup>
-            <div>
-              {entity.aisData.name && <h2>{entity.aisData.name}</h2>}
-              <h3>{entity.aisData.mmsi}</h3>
-            </div>
-          </Popup>
-        </Marker>
-      );
-    }
-    return null;
-  }).filter(Boolean);
+    })
+    .filter(Boolean);
 
   return (
     <div>
@@ -69,6 +77,39 @@ const MapView = (props) => {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+
+        <LayersControl position="topright">
+          <LayersControl.BaseLayer checked name="Standard">
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="Muted">
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="Dark">
+            <TileLayer
+              attribution="Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL."
+              url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="Terrain">
+            <TileLayer
+              attribution="Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL."
+              url="http://{s}.tile.stamen.com/terrain-background/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="Terrain with Roads">
+            <TileLayer
+              attribution="Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL."
+              url="http://{s}.tile.stamen.com/terrain/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+        </LayersControl>
       </MapContainer>
     </div>
   );
