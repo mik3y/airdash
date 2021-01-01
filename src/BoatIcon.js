9ea92f790cf566ec6b6bbcb560679d6a6c583841
mike wakerly <opensource@hoho.com>
2021-01-01 14:22:56 -0500

    ais: color ships according to status

--- src/BoatIcon.js
+++ src/BoatIcon.js
@@ -2,23 +2,46 @@ import L from "leaflet";
 import Boat from "./images/boat.svg";
 import ReactDOMServer from "react-dom/server";
 
+import './BoatIcon.scss'
+
 const STYLE = {
   width: 32,
   height: 32,
 };
 
-const BoatIcon = (vessel) => {
-  const rotation = vessel.courseOverGround || 0;
+const getStyleLabel = (aisData) => {
+  switch (aisData.navigationalStatus) {
+    case 'NAVIGATIONAL_STATUS_UNDERWAY_ENGINE':
+    case 'NAVIGATIONAL_STATUS_UNDERWAY_SAILING':
+    case 'NAVIGATIONAL_STATUS_TOWING_ASTERN':
+    case 'NAVIGATIONAL_STATUS_PUSHING_AHEAD':
+      return 'is-moving';
+    case 'NAVIGATIONAL_STATUS_AT_ANCHOR':
+      return 'is-anchored';
+    case 'NAVIGATIONAL_STATUS_MOORED':
+      return 'is-moored';
+    case 'NAVIGATIONAL_STATUS_AIS_SART':
+      return 'is-sart';
+    default:
+      break;
+  }
+
+  return '';
+}
+
+const BoatIcon = (aisData) => {
+  const rotation = aisData.courseOverGround || 0;
   const style = {
     ...STYLE,
     transform: `rotate(${rotation}deg)`,
   };
+  const styleLabel = getStyleLabel(aisData);
   const icon = L.divIcon({
     className: "custom-icon",
     popupAnchor: [16, -16],
     html: ReactDOMServer.renderToString(
-      <div key={`icon-${vessel.mmsi}`} className="plane-icon">
-        <Boat style={style} />
+      <div key={`icon-${aisData.mmsi}`} className="boat-icon">
+        <Boat style={style} className={styleLabel} />
       </div>
     ),
   });
